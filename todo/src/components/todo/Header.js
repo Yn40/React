7a91e0432fcc1a755a9f42e54cb3336341b58411
Yn <lyn6264@gomcorp.com>
2021-04-07 12:08:06 +0900

    스타일 추가

--- todo/src/components/todo/Header.js
+++ todo/src/components/todo/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTodoState } from 'TodoContext';
+import firecracker from 'img/firecracker.png';
 
 const Header = () => {
   const date = new Date();
@@ -8,7 +9,7 @@ const Header = () => {
     date : date.getFullYear() +"."+ (date.getMonth()+1) +"."+ date.getDate(),
     day : week[date.getDay()]
   };
-  const state = useTodoState();
+  const state = useTodoState(); 
   let yet = 0;
   state.map(todo => !todo.done ? yet++ : yet);
 
@@ -16,6 +17,9 @@ const Header = () => {
     <header>
       <h1>{today.date} <span className="day">{today.day}</span></h1>
       <p className="cnt">총 <b>{state.length}</b>개 - <b>{yet}</b>개 남음</p>
+      <img src={firecracker} alt="폭죽" className={"img_fire "+((yet === 0 && state.length!==0)?"ac":"")}/>
+      <img src={firecracker} alt="폭죽" className={"img_fire "+((yet === 0 && state.length!==0) === 0?"ac1":"")}/>
+      
     </header>
   );
 };
